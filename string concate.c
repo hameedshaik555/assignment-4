eb1f544dcd6ea879785033d94caf5216ccde59a1
hameedshaik555 <88450801+hameedshaik555@users.noreply.github.com>
2021-08-19 23:39:34 +0530

    Create string concate.c

---
+++ string concate.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <string.h>
+ 
+void stringconcatenate(char *s1,char *s2) //using function 
+{
+	int i;
+	int j=strlen(s2); //coverting char to strlengh 
+	
+    for(i=0;s2[i];i++)   f//or iteration loop
+    {
+    	s1[i+j]=s2[i]; //adding the one length to another 
+	}
+	s1[i+j]='\0';
+    
+ 	
+}
+int main()
+{
+ 
+    char s1[1000],s2[1000];   int length ;
+ 
+    printf("Enter  string1: ");
+    gets(s1);
+    printf("Enter  string2: ");
+    gets(s2);
+    stringconcatenate(s1,s2);
+length = strlen(s1) //calculate the length by lenfunction 
+    printf("combined two strings ='%s'\n",s1);
+ 
+     printf("length of the string  ='%s'\n",s1);
+ 
+    return 0;
+    
+}
